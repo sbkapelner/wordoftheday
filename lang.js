ed76c32e272e77c6dee2bdd0731dda1e1b307601
Sarah Kapelner <sbkapelner@gmail.com>
2021-08-14 14:28:24 -0600

    initial commit

---
+++ lang.js
@@ -0,0 +1,10 @@
+var twilio = require('twilio');
+var config = require('./config')
+
+var client = new twilio(config.TWILIO_ACCOUNT_SID, config.TWILIO_AUTH_TOKEN);
+
+client.messages.create({
+    to: config.MY_NUMBER,
+    from: config.MY_TWILIO_NUMBER,
+    body: 'Hello World!'
+});
